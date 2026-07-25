0487b085d59a756c31bdb21686ff15ff6532312b
ishaankad <isha.ankad29@gmail.com>
2026-07-25 00:15:12 -0500

    Dante works with the addition of amp var

--- trial_dante.py
+++ trial_dante.py
@@ -15,7 +15,7 @@ import arviz as az
 #import default_style
 #from sklearn.metrics import r2_score
 # from trial_noise import synthetic_planckian,synthetic_brems
-
+import pytensor.tensor as pt
 
 #%%
 
@@ -40,7 +40,6 @@ def synthetic_planckian(photon_energy_ev, T):
     B = ((num/den) * V)/(D**2)
     return B
 
-
 '''Important note: i must convert the bremsstahlung data from emittivity to irradiance prior to plotting it '''
 
 
@@ -62,7 +61,7 @@ def synthetic_brems(photon_energy_ev, T): # change formula
     term1 = (8/3)*(((2*np.pi) / (3*m_e*k_b*T)) ** 0.5)
     term2 = (e_c**6) / (m_e * (c**3))
     term3 = (Z**2)*n_e*n_i
-    term4 = np.exp(-E_p / E_t)
+    term4 = pm.math.exp(-E_p / E_t)
     gaunt_factor = 1 + (0.1728) * (((E_p)/(I_h * (Z**2))) ** (1/3)) * (1 + ((2 * E_t) / (E_p)))
     j = (term1 * term2 * term3 * term4 * gaunt_factor)
     irr_cgs = (j*V)/(D**2)
@@ -81,7 +80,7 @@ def synthetic_brems(photon_energy_ev, T): # change formula
 # df = pd.read_csv('86459_time_2.0_spectrum.csv')
     
 time_step = [1.0,1.5,2.0,2.5,3.0,3.5]
-str(time_step)
+# str(time_step)
 
 fig,ax = plt.subplots()
 
@@ -97,7 +96,7 @@ for time in time_step:
             
     #69 degrees
     area_h = np.pi * (0.6e-3)**2
-    area_h * np.cos(np.radians(69))
+    # area_h * np.cos(np.radians(69))
     power_W = power_GW * 1e9
     
     observed_data = np.pi * power_W / area_h
@@ -110,24 +109,39 @@ for time in time_step:
     if __name__ == '__main__':
     
         '''creating prediction model'''
+        
         with pm.Model() as Model:
             # x = pos_PE
             # y = y_scaled
             mask = given_PE > 0.5   
             x = given_PE[mask]
             y = y_scaled[mask]
+
+            # test_model = synthetic_planckian(x, 100) + synthetic_brems(x, 170)
+            # print("model magnitude:", np.max(test_model), np.min(test_model))
+            # print("data magnitude (unscaled):", np.max(observed_data), np.min(observed_data))
+                        
             T_guess = 100 #[eV]
             T_guess_brems = 170 #[eV]
             
-            T_dist = pm.TruncatedNormal('T', mu=T_guess, sigma=50, lower=5)
-            T_dist_brems = pm.TruncatedNormal('T_brems', mu=T_guess_brems, sigma=20, lower=5)
+            # T_dist = pm.TruncatedNormal('T', mu=T_guess, sigma=50, lower=5)
+            # T_dist_brems = pm.TruncatedNormal('T_brems', mu=T_guess_brems, sigma=20, lower=5)
+            
+            log_T = pm.Normal('log_T', mu=np.log(T_guess), sigma=0.7)
+            T_dist = pm.Deterministic('T', pm.math.exp(log_T))
+            
+            log_T_brems = pm.Normal('log_T_brems', mu=np.log(T_guess_brems), sigma=0.5)
+            T_dist_brems = pm.Deterministic('T_brems', pm.math.exp(log_T_brems))
+            
+            log_amp = pm.Normal('log_amp', mu=0, sigma=10)
+            amp = pm.math.exp(log_amp)
             
             model = synthetic_planckian(x,T_dist)
             model_brems = synthetic_brems(x,T_dist_brems) 
             
             model_both = model + model_brems
             noise = pm.HalfNormal('noise', sigma=0.1) + 1e-5
-            model_both_scaled = model_both/scale_coef
+            model_both_scaled = amp * model_both / pt.max(model_both)
     
             y_pred = pm.Normal('y_pred', mu=model_both_scaled, sigma=noise, observed=y) 
             
